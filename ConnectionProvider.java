17a2593ddfcd3c682a5f6991ea7de3f5943c61a7
Hari Pranesh <74141632+haripranesh1505@users.noreply.github.com>
2021-07-12 20:50:19 +0530

    Add files via upload

---
+++ ConnectionProvider.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+import java.sql.*;
+/**
+ *
+ * @author 91986
+ */
+public class ConnectionProvider {
+    public static Connection getCon()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/qems","root","12345");
+            return con;
+        }
+        catch(Exception e)
+        {
+            return null;
+        }
+    }
+}

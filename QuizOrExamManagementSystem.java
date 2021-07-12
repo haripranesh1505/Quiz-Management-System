75d2f2e4fa31f96b70213c4bb72caa5239332338
Hari Pranesh <74141632+haripranesh1505@users.noreply.github.com>
2021-07-12 20:42:41 +0530

    Add files via upload

---
+++ QuizOrExamManagementSystem.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz.or.exam.management.system;
+
+/**
+ *
+ * @author 91986
+ */
+public class QuizOrExamManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
